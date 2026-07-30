371c0d39c0187cf622dd5b2036b076937ea82003
JKumar1603 <jaykumar16102@gmail.com>
2026-07-30 16:28:04 +0530

    Remove demo folder

--- demo/auth-service/src/main/java/com/completebank/auth/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.completebank.auth.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
